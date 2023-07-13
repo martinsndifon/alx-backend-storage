e3d092430d4de6ef08c5dd339ca3c14172937da0
Martins Ndifon <martinsndifon@gmail.com>
2023-07-13 17:55:56 +0000

    Added an informatory comment on the file

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,5 @@
 -- creates a trigger that resets the attribute 'valid_email' only when the 'email' has been changed.
+-- <> is same as != why did I use <>? So I can remember both
 DELIMITER //
 CREATE TRIGGER email_validation BEFORE UPDATE ON users
 FOR EACH ROW

f91240fef61dd061abc626b410eff4da849eaab0
Martins Ndifon <martinsndifon@gmail.com>
2023-07-19 18:37:16 +0000

    update to task-1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,17 +2,17 @@
 """ALX SE"""
 import redis
 import uuid
-from typing import Any
+from typing import Any, Callable, Union, Optional
 
 
 class Cache:
     """Interact with the redis db"""
-    def __init__(self):
+    def __init__(self) -> None:
         """store an instance of the Redis client and flush the instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         generates a random key and store the store the input data in Redis
         using the random key, returns the key
@@ -20,3 +20,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """
+        converts redis return value type into the correct type
+        """
+        value = self._redis.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_int(self, data: bytes) -> int:
+        """converts the type from bytes to int"""
+        return int(data)
+
+    def get_str(self, data: bytes) -> str:
+        """converts the type from bytes to int"""
+        return data.decode('utf-8')
+
